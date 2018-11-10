94b7a4a4506f048bde69cf4bf9b66fe03151dfc7
Michael Equi <32988490+Michael-Equi@users.noreply.github.com>
2018-11-09 21:29:43 -0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,8 +9,8 @@ sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/buildPatchedKernel.sh"
 echo "RESTART SYSTEM"
 
 #sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/installLibrealsense.sh"
-#sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/scripts/patchKernel.sh"
-#sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/scripts/configureKernel.sh"
+#sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/scripts/patchKernel.sh" #DON'T USE
+#sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/scripts/configureKernel.sh" #DON'T USE
 #sudo "${MY_PATH}/jetsonhacks/installROSTX2/installROS.sh -p ros-kinetic-desktop"
 #sudo "${MY_PATH}/jetsonhacks/installROSTX2/setupCatkinWorkspace.sh"
 #sudo "${MY_PATH}/jetsonhacks/installRealSense2ROSTX/installRealSenseROS.sh jetsoncar_ws"
