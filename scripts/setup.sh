0380225e413db31ca57ca48593ef697ac60a9af3
Michael's TX2 <equim20@student.jhs.net>
2018-11-10 05:27:37 +0000

    Updated setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,11 +3,12 @@
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
 
-
-sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/installLibrealsense.sh"
-sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/scripts/patchKernel.sh"
-sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/scripts/configureKernel.sh"
-sudo "${MY_PATH}/jetsonhacks/installROSTX2/installROS.sh -p ros-kinetic-desktop"
+sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/buildPatchedKernel.sh"
+echo "RESTART SYSTEM"
+#sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/installLibrealsense.sh"
+#sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/scripts/patchKernel.sh"
+#sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/scripts/configureKernel.sh"
+#sudo "${MY_PATH}/jetsonhacks/installROSTX2/installROS.sh -p ros-kinetic-desktop"
 #sudo "${MY_PATH}/jetsonhacks/installROSTX2/setupCatkinWorkspace.sh"
-sudo "${MY_PATH}/jetsonhacks/installRealSense2ROSTX/installRealSenseROS.sh ${MY_PATH}/../jetsoncar_ws"
+#sudo "${MY_PATH}/jetsonhacks/installRealSense2ROSTX/installRealSenseROS.sh jetsoncar_ws"
 
