bda8b2ebe5cb9ef5a48a817f90e7aa23db80dfbc
Michael Equi <32988490+Michael-Equi@users.noreply.github.com>
2018-11-09 21:26:04 -0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,9 @@
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
 
+#Requires update in order to properly source enviroment vars
+sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/installLibrealsense.sh" #RESTART Needed after install! Check if it can be held off until full installation finishes
 
-sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/installLibrealsense.sh"
 sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/scripts/patchKernel.sh"
 sudo "${MY_PATH}/jetsonhacks/buildLibrealsense2TX/scripts/configureKernel.sh"
 sudo "${MY_PATH}/jetsonhacks/installROSTX2/installROS.sh -p ros-kinetic-desktop"
