99e9eed37d17bdcd0fb92a14fcdb268f5b413395
Michael's TX2 <equim20@student.jhs.net>
2018-11-19 12:24:43 -0800

    Updated teleop to be able to switch steering direction

--- jetsoncar_ws/src/racecar_description/src/joy_teleop.py
+++ jetsoncar_ws/src/racecar_description/src/joy_teleop.py
@@ -4,16 +4,26 @@ import rospy
 from sensor_msgs.msg import Joy
 from ackermann_msgs.msg import AckermannDriveStamped
 
+lastSteeringState = 0
+steeringState = -1
+
 def JoyCB(data):
 
+    global lastSteeringState
+    global steeringState
+
+    if data.buttons[2] != lastSteeringState and lastSteeringState == 0:
+	lastButton = data.buttons[2]
+	steeringState = -1*steeringState
+        
     msg = AckermannDriveStamped();
     msg.header.stamp = rospy.Time.now()
     msg.header.frame_id = "base_link"
 
-    msg.drive.speed = data.axes[1]/5 #x/5 makes max throttle 2/10
     msg.drive.acceleration = 1;
+    msg.drive.speed = data.axes[1]/5 #x/5 makes max throttle 2/10
     msg.drive.jerk = 1;
-    msg.drive.steering_angle = data.axes[0]
+    msg.drive.steering_angle = data.axes[0]*steeringState
     msg.drive.steering_angle_velocity = 1
 
     pub.publish(msg)
