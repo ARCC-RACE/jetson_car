b4721c72740565efad0f99b2e4bbc73f788f0b5e
Michael Equi <equim20@student.jhs.net>
2018-11-18 18:42:51 -0800

    Added a temporary hardware interface until contorl loop is established

---
+++ jetsoncar_ws/src/temp_racecar_hardware_interface/include/racecar_hardware_interface/racecar_arduino_com.h
@@ -0,0 +1,45 @@
+#ifndef RACECAR_ARDUINO_COM_H
+#define RACECAR_ARDUINO_COM_H
+
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <cstdio>
+#include <stdint.h>
+#include <ros/ros.h>
+
+#include "serial/serial.h"
+
+
+enum Joint // Corresponds the the index in the arrays passed in by hardware_interface
+{
+  LEFT_REAR_WHEEL,     // Starting at 0
+  RIGHT_REAR_WHEEL,
+  LEFT_FRONT_WHEEL,
+  RIGHT_FRONT_WHEEL,
+  LEFT_STEERING_HINGE,
+  RIGHT_STEERING_HINGE,
+};
+
+
+class HardwareCom
+{
+  //Serial object that manages USB serial connection
+  serial::Serial connection;
+
+  //Packet structure = |PEC|steering MSB|steering LSB|throttle MSB|throttle LSB|
+  uint8_t packetDown[5]; //Packet to the arduino (length = 4 + PEC)
+
+  uint8_t packetUp[4];     //Packet from the arduino (length = 4) (unused)
+  double steeringPosition; //Member var for holding "actual" steering angle
+  double wheelVelocity;     //Member var for holding "actual" wheel velocity
+
+
+public:
+  HardwareCom(std::string port, int baud);     //Constructor
+  void setController(int length, double* cmd); //Sends input commands to arduino
+  double readController(Joint joint);          //Returns data read from the arduino
+
+};
+
+#endif
---
+++ jetsoncar_ws/src/temp_racecar_hardware_interface/src/temp_racecar_hardware_interface.cpp
@@ -0,0 +1,71 @@
+#include <sensor_msgs/JointState.h>
+#include <std_msgs/Float64.h>
+
+#include "racecar_hardware_interface/racecar_arduino_com.h"
+
+double steering = 0;
+double throttle = 0;
+
+std::string joints[6] = {"left_front_wheel_joint", "left_rear_wheel_joint", "right_front_wheel_joint",
+  "right_rear_wheel_joint","left_steering_hinge_joint", "right_steering_hinge_joint"};
+
+double cmd[6] = {0,0,0,0,0,0}; //stores values to update car
+
+void updateThrottle(const std_msgs::Float64::ConstPtr& input){
+  throttle = input->data;
+}
+
+void updateSteering(const std_msgs::Float64::ConstPtr& input){
+  steering = input->data;
+}
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "temp_racecar_hardware_interface");
+  ros::NodeHandle nh;
+
+  HardwareCom connection("/dev/ttyUSB0", 115200);
+
+  ros::Subscriber left_rear_wheel_pub = nh.subscribe("/racecar/left_front_wheel_velocity_controller/command", 1, updateThrottle);
+  ros::Subscriber left_steering_hinge_pub = nh.subscribe("/racecar/left_steering_hinge_position_controller/command", 1, updateSteering);
+
+  ros::Publisher JointStatePub = nh.advertise<sensor_msgs::JointState>("/racecar/joint_states", 1);
+
+  ros::Rate rate(50); //50Hz
+
+  while(ros::ok()){
+
+    //Update command array
+    cmd[0] = throttle;
+    cmd[1] = throttle;
+    cmd[2] = throttle;
+    cmd[3] = throttle;
+    cmd[4] = steering;
+    cmd[5] = steering;
+
+    connection.setController(6, cmd); //Send values to arduino
+
+    //Update joint states
+    sensor_msgs::JointState joint_state_msg;
+
+    for(int i = 0; i < 6; i++){
+      joint_state_msg.name.push_back(joints[i]); //Treat as vectors
+    }
+    for(int i = 0; i < 4; i++){ //Increment through throttle joints
+      joint_state_msg.position.push_back(0);
+      joint_state_msg.velocity.push_back(throttle);
+      joint_state_msg.effort.push_back(0);
+    }
+    for(int i = 4; i < 6; i++){ //Increment through steering joints
+      joint_state_msg.position.push_back(steering);
+      joint_state_msg.velocity.push_back(0);
+      joint_state_msg.effort.push_back(0);
+    }
+
+    JointStatePub.publish(joint_state_msg);
+
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  return 0;
+}
