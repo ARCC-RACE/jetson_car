2fde5b5e90ab21f9a97b05b18e4191b92c177ce6
Michael's TX2 <equim20@student.jhs.net>
2018-11-19 12:24:43 -0800

    Updated teleop to be able to switch steering direction

---
+++ drivers/carHWComTest/CMakeFiles/carHWComTest.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nvidia/Desktop/jetson_car/drivers/carHWComTest/main.cpp" "/home/nvidia/Desktop/jetson_car/drivers/carHWComTest/CMakeFiles/carHWComTest.dir/main.cpp.o"
+  "/home/nvidia/Desktop/jetson_car/drivers/carHWComTest/racecar_arduino_com.cpp" "/home/nvidia/Desktop/jetson_car/drivers/carHWComTest/CMakeFiles/carHWComTest.dir/racecar_arduino_com.cpp.o"
+  "/home/nvidia/Desktop/jetson_car/drivers/carHWComTest/serial.cpp" "/home/nvidia/Desktop/jetson_car/drivers/carHWComTest/CMakeFiles/carHWComTest.dir/serial.cpp.o"
+  "/home/nvidia/Desktop/jetson_car/drivers/carHWComTest/unix.cpp" "/home/nvidia/Desktop/jetson_car/drivers/carHWComTest/CMakeFiles/carHWComTest.dir/unix.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
