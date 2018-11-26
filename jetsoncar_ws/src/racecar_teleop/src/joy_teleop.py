9da3681bc103d9c0c88d4ed806f407db6e2f506a
Michael Equi <equim20@student.jhs.net>
2018-11-25 20:28:13 -0800

    Fixed controller modes

--- jetsoncar_ws/src/racecar_teleop/src/joy_teleop.py
+++ jetsoncar_ws/src/racecar_teleop/src/joy_teleop.py
@@ -1,46 +1,92 @@
 #!/usr/bin/env python
 
 import rospy
+import math
 from sensor_msgs.msg import Joy
 from ackermann_msgs.msg import AckermannDriveStamped
 
-lastSteeringState = 0
+#BUTTON MAPPING
+# X [2]  = Flip steering
+# Y [3]  = Fine steering mode (-1 to 1 -> -0.5 to 0.5)
+# A [0]  = Switch driving mode (default = RT+LT, alternate = RT-LT)
+# B [1]  = No function
+# LB [4] = Brake
+# RB [5] = Brake
+
+#AXIS MAPPING
+# LT [2] = throttle forward/reverse (-1 to 1)
+# RT [5] = throttle forward (-1 to 1)
+# L [0]  = steering control (-1 to 1)
+# L [1]  = No function
+
+#global vars with default values set
+#vars for flipping steering
+lastSteeringState = 0  #in order to look for rising edge on button
 steeringState = -1
-lastThrottleState = 0
-throttleState = True # True means adding throttles (no reverse), false means reverse enabled
-totalThrottle = 0
+
+#vars for switching driving mode
+lastThrottleState = 0 #in order to look for rising edge on button
+throttleState = True  #true means adding throttles (no reverse), false means reverse enabled
+
+lastFineSteeringState = 0
+fineSteeringState = True #true means -1 to 1, false means -0.5 to 0.5 (rads in simulation)
+
+#helper function to map values
+def map(input, inMin, inMax, outMin, outMax):
+    output = (input - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
+    return output
 
 def JoyCB(data):
 
+    #Use global/static vars
     global lastSteeringState
     global steeringState
+
     global lastThrottleState
     global throttleState
-    global totalThrottle
 
+    global lastFineSteeringState
+    global fineSteeringState
 
-    if data.buttons[2] != lastSteeringState and lastSteeringState == 0:
-         lastSteeringState = data.buttons[2]
-         steeringState = -1*steeringState
+    #steering direction
+    if(data.buttons[2] != lastSteeringState and lastSteeringState == 0):
+         steeringState = -steeringState
+         rospy.logdebug("Changing steering direction")
+    lastSteeringState = data.buttons[2]
 
-    if data.buttons[0] != lastThrottleState and lastThrottleState == 1:
-         lastThrottleState = data.buttons[0]
+    #driving mode
+    if(data.buttons[0] != lastThrottleState and lastThrottleState == 0):
          throttleState = not throttleState
+         rospy.logdebug("Changing driving mode")
+    lastThrottleState = data.buttons[0]
 
+    #fine steering mode
+    if(data.buttons[3] != lastFineSteeringState and lastFineSteeringState == 0):
+        fineSteeringState = not fineSteeringState
+        rospy.logdebug("Changing steering mode")
+    lastFineSteeringState = data.buttons[3]
+
+    #apply modificaitons to throttle based on drive mode selection
     if throttleState:
-         totalThrottle = (data.axes[2] + data.axes[5] - 2) * -1
+         throttleVal = map(data.axes[5], 1, -1, 0, 2) + map(data.axes[2], 1, -1, 0, 2) #range is 0 to 4
     else:
-         totalThrottle = data.axes[2] - data.axes[5]
+         throttleVal = map(data.axes[5], 1, -1, 0, 2) - map(data.axes[2], 1, -1, 0, 2) #range is -2 to 2
+
+    #apply modifications to steering based on steering mode selection
+    if fineSteeringState:
+        steeringVal = data.axes[0]   #range is -1 to 1
+    else:
+        steeringVal = data.axes[0]/2 #range is -0.5 to 0.5
 
     msg = AckermannDriveStamped();
     msg.header.stamp = rospy.Time.now()
     msg.header.frame_id = "base_link"
 
-    msg.drive.acceleration = 1;
-    msg.drive.speed = totalThrottle/5 #x/5 makes max throttle 2/10
-    msg.drive.jerk = 1;
-    msg.drive.steering_angle = data.axes[0]*steeringState
-    msg.drive.steering_angle_velocity = 1
+    msg.drive.acceleration = 0; #drive acceleration unknown
+    msg.drive.speed = throttleVal
+    msg.drive.jerk = 0; #jerk unknown
+    msg.drive.steering_angle = steeringVal*steeringState #steering state is 1 or -1 to flip steering if needed
+    msg.drive.steering_angle_velocity = 0 #angle velocity unknown
 
     pub.publish(msg)
 
@@ -48,4 +94,4 @@ rospy.init_node("joy_teleop")
 rospy.Subscriber("joy", Joy, JoyCB)
 pub = rospy.Publisher('/racecar/ackermann_cmd', AckermannDriveStamped, queue_size=1)
 
-rospy.spin()
+rospy.spin() #keep node from closing
