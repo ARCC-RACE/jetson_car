3731d3e7679f05eedd157ad5b43b6141dae94e32
Michael's TX2 <equim20@student.jhs.net>
2019-07-01 14:53:13 -0700

    fixed more bugs

--- jetsoncar_ws/src/drivers/nn_tools/src/racecarDataCollector.py
+++ jetsoncar_ws/src/drivers/nn_tools/src/racecarDataCollector.py
@@ -10,6 +10,7 @@ from ackermann_msgs.msg import AckermannDriveStamped
 from std_msgs.msg import Bool
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import Imu
+from bno055.msg import bno055_info
 import csv
 import random #To randomly sort files into test and train folders (20% test, 80% train)
 
@@ -24,7 +25,7 @@ class RacecarDataCollector():
         self.include_imu= include_imu
 
         self.isRecording = False #by default the system is not recording
-        self.dir = os.path.join(os.path.join("/media/", getpass.getuser()), "/racecarDataset") #directory of expected USB flashdrive
+        self.dir = os.path.join(os.path.join("/media/", getpass.getuser()), "racecarDataset") #directory of expected USB flashdrive
         self.image_color_encoding = image_color_encoding
 
         self.lastRead = rospy.get_time() #get seconds in a float value
@@ -38,15 +39,15 @@ class RacecarDataCollector():
         #looking for usb flash drive with name racecarDataset
         if os.path.exists(self.dir):
             try:
-                os.makedirs(os.path.join(self.dir, "/dataset/training_set"))
-                os.makedirs(os.path.join(self.dir, "/dataset/test_set"))
+                os.makedirs(os.path.join(self.dir, "dataset/training_set"))
+                os.makedirs(os.path.join(self.dir, "dataset/test_set"))
                 rospy.loginfo("Dataset directory created")
                 #make the csv files
                 #prepare CSV files by writing header (starts with blank csv)
-                with open(os.path.join(self.dir, "/dataset/training_set/tags.csv"), 'w') as csvfile: #csv in train folder
+                with open(os.path.join(self.dir, "dataset/training_set/tags.csv"), 'w') as csvfile: #csv in train folder
                     csv_writer = csv.DictWriter(csvfile, self.feildNames)
                     csv_writer.writeheader()
-                with open(os.path.join(self.dir, "/dataset/test_set/tags.csv"), 'w') as csvfile: #csv in test folder
+                with open(os.path.join(self.dir, "dataset/test_set/tags.csv"), 'w') as csvfile: #csv in test folder
                     csv_writer = csv.DictWriter(csvfile, self.feildNames)
                     csv_writer.writeheader()
             except OSError:
@@ -66,13 +67,13 @@ class RacecarDataCollector():
         rospy.Subscriber(rgb_image_topic, Image, self.newImage) #video
         rospy.Subscriber(depth_image_topic, Image, self.newDepth) #depth
         rospy.Subscriber(imu_topic, Imu, self.newIMUData) #IMU
-        rospy.Subscriber(imu_calibration_topic, Imu, self.newIMUCalibData) #IMU info/calibration
+        rospy.Subscriber(imu_calibration_topic, bno055_info, self.newIMUCalibData) #IMU info/calibration
 
     #record data with time stamp
     def newImage(self, new_image):
         timeStamp = rospy.get_time()
 
-        if(isRecording and timeStamp - self.lastRead > 1.0/collection_fps): # fps data recording (delay = 1/desired fps)
+        if(self.isRecording and timeStamp - self.lastRead > 1.0/collection_fps): # fps data recording (delay = 1/desired fps)
 
             try:
                 bridge = CvBridge() #convert ros image to cv compatible image
@@ -86,7 +87,7 @@ class RacecarDataCollector():
 
             #create a new row to add into a csv file
             if self.include_imu:
-                new_data = [str(timeStamp), str(self.lastAckermann.drive.steering_angle), str(self.lastAckermann.drive.speed), str(self.imu_calibdata.sysCalibration), str(self.imu_data.orientation.x),
+                new_data = [str(timeStamp), str(self.lastAckermann.drive.steering_angle), str(self.lastAckermann.drive.speed), str(self.imu_calib_data.sysCalibration), str(self.imu_data.orientation.x),
                     str(self.imu_data.orientation.y), str(self.imu_data.orientation.z), str(self.imu_data.orientation.w), str(self.imu_data.angular_velocity.x), str(self.imu_data.angular_velocity.y),
                     str(self.imu_data.angular_velocity.z), str(self.imu_data.linear_acceleration.x), str(self.imu_data.linear_acceleration.y), str(self.imu_data.linear_acceleration.z)]
             else:
@@ -95,20 +96,20 @@ class RacecarDataCollector():
             fileSelector = random.randint(0, 10) #random number between 0 and 9 inclusive
             #put new_data into csv
             if(fileSelector < 8): #send data to train folder
-                cv2.imwrite(os.path.join(os.path.join(self.dir, "/dataset/training_set/"), str(timeStamp) + ".jpg"), image) #write image into train folder
+                cv2.imwrite(os.path.join(os.path.join(self.dir, "dataset/training_set/"), str(timeStamp) + ".jpg"), image) #write image into train folder
                 if self.include_depth:
-                    cv2.imwrite(os.path.join(os.path.join(self.dir, "/dataset/training_set/"), str(timeStamp) + "_depth.jpg"), depth) #write depth image into train folder
+                    cv2.imwrite(os.path.join(os.path.join(self.dir, "dataset/training_set/"), str(timeStamp) + "_depth.jpg"), depth) #write depth image into train folder
                 #update csv file
-                with open(os.path.join(self.dir, "/dataset/training_set/tags.csv"), 'a') as csvfile: # append to csv file
+                with open(os.path.join(self.dir, "dataset/training_set/tags.csv"), 'a') as csvfile: # append to csv file
                     csv_writer = csv.writer(csvfile, self.feildNames)
                     csv_writer.writerow(new_data)
 
             else: #send data to test folder
-                cv2.imwrite(os.path.join(os.path.join(self.dir, "/dataset/test_set/"), str(timeStamp) + ".jpg"), image) #write image into test folder
+                cv2.imwrite(os.path.join(os.path.join(self.dir, "dataset/test_set/"), str(timeStamp) + ".jpg"), image) #write image into test folder
                 if self.include_depth:
-                    cv2.imwrite(os.path.join(os.path.join(self.dir, "/dataset/test_set/"), str(timeStamp) + "_depth.jpg"), depth) #write depth image into test folder
+                    cv2.imwrite(os.path.join(os.path.join(self.dir, "dataset/test_set/"), str(timeStamp) + "_depth.jpg"), depth) #write depth image into test folder
                 #update csv file
-                with open(os.path.join(self.dir, "/dataset/test_set/tags.csv"), 'a') as csvfile: # append to csv file
+                with open(os.path.join(self.dir, "dataset/test_set/tags.csv"), 'a') as csvfile: # append to csv file
                     csv_writer = csv.writer(csvfile, self.feildNames)
                     csv_writer.writerow(new_data)
 
@@ -137,14 +138,10 @@ class RacecarDataCollector():
 
 if __name__=="__main__":
     #program starts running here
-    print(":)")
     rospy.init_node("data_collector")
-    print("Hi")
     include_imu = rospy.get_param('~include_imu', False)
     include_depth = rospy.get_param('~include_depth', False)
     collection_fps = rospy.get_param('~collection_fps', 15)
     image_color_encoding = rospy.get_param('~image_color_encoding', "bgr8")
-    print("Hello")
     data_collector = RacecarDataCollector(collection_fps, include_depth, include_imu, image_color_encoding=image_color_encoding)
-    print("Here")
     rospy.spin() #keep node running while callbacks are handled
