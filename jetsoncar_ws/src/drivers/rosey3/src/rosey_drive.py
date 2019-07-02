07c8f19873e7afbc9453c2c63ccf5a970cca8422
Michael Equi <equim20@student.jhs.net>
2019-07-01 21:27:40 -0700

    Added updates for temporal series in rosey_drive.py for rosey3

--- jetsoncar_ws/src/drivers/rosey3/src/rosey_drive.py
+++ jetsoncar_ws/src/drivers/rosey3/src/rosey_drive.py
@@ -16,6 +16,12 @@ from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float64
 
+#var for storing numpy array sent to neural network (3*num_images_to_stack channels because it is 3 RGB images stacked together in axis=2)
+num_images_to_stack = 3
+input_shape = list(utils.INPUT_SHAPE)
+input_shape[2] = utils.INPUT_SHAPE[2]*num_images_to_stack
+images = np.empty(input_shape)
+
 # callback that runs when a new image is recieved from realsense camera
 def newImage(new_image):
     timeStamp = rospy.get_time()
@@ -23,6 +29,8 @@ def newImage(new_image):
     global lastRead
     global steering_prediction
     global rosey #CNN model
+    global images
+
     if(timeStamp - lastRead > 0.033333333): # ~30 fps desired data flow (1/desired fps)
 
         try:
@@ -39,9 +47,11 @@ def newImage(new_image):
         image = utils.preprocess(image) # preprocess image (crop, resize, rgb2yuv)
 #        cv2.imshow("image", image)         #DEBUGGING
 #        cv2.waitKey(0)                     #DEBUGGING
-
-        image = np.array([image]) # give the model a 4D array
-        steering_prediction = float(rosey.predict(image, batch_size=1))
+        #remove oldest image and input new one
+        images = np.roll(images,3,axis=2)
+        images[:,:,0:2] = image
+        images = np.array([images]) # give the model a 4D array
+        steering_prediction = float(rosey.predict(images, batch_size=1))
 #        processTime = rospy.get_time() - startProcessing #DEBUGGING
 #        print(processTime) # prints time to process image and make prediction in seconds
         rospy.logdebug("Steering prediction from Rosey: %f", steering_prediction)
